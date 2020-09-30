f053e3463da1b7f5c87fccf31573a37bb8bf127e
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 19:41:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,16 +12,13 @@ end
   
   
   def using_pop(continents)
-    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+    continents.pop
   end
 
 
 
 def pop_with_args(dog_breeds)
- 
-    dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    pop_with_args = dog_breeds.pop(2)
+ pop_with_args = dog_breeds.pop(2)
 end
 
 def using_shift(my_favorite_cities) 
