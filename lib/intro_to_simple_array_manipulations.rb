8458c12b3bc5f3e939d321c8edcdc8718d3bc72e
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 19:12:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,13 +27,13 @@ end
 def using_shift(my_favorite_cities) 
   
     my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-    my_favorite_cities.shift
+    neverbeencities = my_favorite_cities.shift
   end
   
 
 def shift_with_args(ice_cream_brands)
  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    ice_cream_brands.shift(2)
+    fat = ice_cream_brands.shift(2)
   end
   
  
