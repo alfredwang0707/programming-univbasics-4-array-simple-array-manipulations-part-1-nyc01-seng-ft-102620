f7d3ed68b8c64ec54b586a107d75bff4ca6728ff
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 19:02:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(colors_in_the_rainbow, next_color)
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+   next_color = "violet"
+   colors_in_the_rainbow.push(next_color)
+end
+
+ def using_unshift(bouroughs_in_nyc, new_neighborhood)
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
+    bouroughs_in_nyc.unshift(new_neighborhood)
+  end
+  
+  
+  def using_pop(continents)
+    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+    continents.pop
+  end
+
+
+
+def pop_with_args(dog_breeds)
+ 
+    dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities) 
+  
+    my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+    my_favorite_cities.shift
+  end
+  
+
+describe "shift_with_args" do 
+  before(:each) do 
+    @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    @brands_removed = shift_with_args(@ice_cream_brands)
+  end
+  
+  it "takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array" do 
+    expect(@brands_removed).to eq(["Blue Bell Creameries", "Ben & Jerry's"])
+  end
+
+  it "decreases the length of the array by 2" do 
+    expect(@brands_removed.size).to eq(2)
+  end
+end
